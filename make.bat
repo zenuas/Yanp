fc6786f928e1e14c763e1ee3e815c0c486c60d71
zenuas@gmail.com <zenuas@gmail.com>
2023-01-15 11:58:46 +0900

    コマンド整理

--- make.bat
+++ make.bat
@@ -1,19 +1,39 @@
-@echo off
+@setlocal enabledelayedexpansion
+@set PREVPROMPT=%PROMPT%
+@prompt $E[1A
+@set MAKE=make.bat
+@echo on
 
-if "%1" == "" (set TARGET=build
+@if "%1" == "" (set TARGET=build
 ) else (set TARGET=%1 && shift)
 
-call :%TARGET% %*
-exit /b %ERRORLEVEL%
+@call :%TARGET% %1 %2 %3 %4 %5 %6 %7 %8 %9
+@prompt %PREVPROMPT%
+@exit /b %ERRORLEVEL%
 
 :build
 	dotnet build --nologo -v q --clp:NoSummary
-	exit /b %ERRORLEVEL%
+	@exit /b %ERRORLEVEL%
 
 :clean
 	dotnet clean --nologo -v q
-	exit /b %ERRORLEVEL%
+	@exit /b %ERRORLEVEL%
+
+:distclean
+	@call :clean
+	rmdir /S /Q src\bin  2>nul
+	rmdir /S /Q src\obj  2>nul
+	rmdir /S /Q test\bin 2>nul
+	rmdir /S /Q test\obj 2>nul
+	@exit /b %ERRORLEVEL%
+
+:release
+	dotnet publish src --nologo -v q --clp:NoSummary -c Release -o .tmp
+	git archive HEAD --output=Yanp-%DATE:/=%.zip
+	powershell -NoProfile Compress-Archive -Force -Path .tmp\yanp.exe, .tmp\yanp.dll, .tmp\yanp.runtimeconfig.json, README.md, LICENSE -DestinationPath Yanp-bin-%DATE:/=%.zip
+	rmdir /S /Q .tmp 2>nul
+	@exit /b %ERRORLEVEL%
 
 :test
 	dotnet test --nologo -v q
-	exit /b %ERRORLEVEL%
+	@exit /b %ERRORLEVEL%
