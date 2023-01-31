7b88954c3926cff3a82dcafb5018bff3944a2480
zenuas@gmail.com <zenuas@gmail.com>
2023-01-31 21:36:16 +0900

    デフォルト指定を付与

--- src/Data/Syntax.cs
+++ src/Data/Syntax.cs
@@ -8,7 +8,7 @@ public class Syntax
     public StringBuilder HeaderCode { get; } = new();
     public string FooterCode { get; set; } = "";
     public string Start { get; set; } = "";
-    public string Default { get; set; } = "";
+    public string Default { get; set; } = "object";
 
     public Dictionary<string, string> Defines { get; } = new();
 
