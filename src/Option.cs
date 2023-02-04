d7f4879b1da0b17712827a9efa6e046097910603
zenuas@gmail.com <zenuas@gmail.com>
2023-02-04 12:58:20 +0900

    helpとversionコマンド追加

--- src/Option.cs
+++ src/Option.cs
@@ -1,6 +1,7 @@
 ﻿using Command;
 using System;
 using System.IO;
+using System.Reflection;
 
 namespace Yanp;
 
@@ -25,4 +26,37 @@ public class Option
     [ShortOption('o')]
     [LongOption("output")]
     public string Output { get; set; } = "";
+
+    [ShortOption('h')]
+    [LongOption("help")]
+    public void Help()
+    {
+        Console.WriteLine(
+$@"Usage: {Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location)} [OPTION]... FILE
+Generate LALR(1) parser.
+
+  -i, --input FILE          input syntax definition file
+  -o, --output DIRECTORY    output to DIRECTORY
+  -t, --template DIRECTORY  specify the output programming language
+  -l, --template-level N    specify the output range for template
+
+  -h, --help                this help and exit
+  -V, --version             ersion information and exit
+
+contribute to https://github.com/zenuas/Yanp");
+        Environment.Exit(0);
+    }
+
+    [ShortOption('V')]
+    [LongOption("version")]
+    public void Version()
+    {
+        Console.WriteLine(
+$@"yanp {File.GetLastWriteTime(Assembly.GetExecutingAssembly().Location):d}
+
+Copyright zenuas
+Released under the MIT license
+https://github.com/zenuas/Yanp");
+        Environment.Exit(0);
+    }
 }
