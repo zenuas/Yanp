f00f31d5e93999d4db5275e05ea6a141ad4d47d6
zenuas@gmail.com <zenuas@gmail.com>
2023-02-12 11:45:15 +0900

    必要以上の還元を行ってしまう問題のテストケース作成 テスト未達成

--- test/LALR1Lookahead.cs
+++ test/LALR1Lookahead.cs
@@ -24,7 +24,8 @@ public class LALR1Lookahead
 start : left '=' right 
       | right
 left  : right
-right : ID");
+right : ID
+");
         Assert.Equal(8, nodes.Length);
         Assert.Equal("$ACCEPT : . start $END; start : . left '=' right; left : . right; start : . right; right : . ID", nodes[0].ToString());
         Assert.Equal("$ACCEPT : start . $END", nodes[1].ToString());
@@ -34,5 +35,31 @@ right : ID");
         Assert.Equal("start : left '=' right . [$END]", nodes[5].ToString());
         Assert.Equal("right : ID . ['=', $END]", nodes[6].ToString());
         Assert.Equal("left : right . ['=']; start : right . [$END]", nodes[7].ToString());
+    }
+
+    [Fact]
+    public void Lookahead2()
+    {
+        var nodes = RunString(@"
+start : a bvoid c
+      | bvoid
+a     : 'A'
+bvoid : void
+      | 'B'
+c     : 'C'
+void  :
+");
+        Assert.Equal(11, nodes.Length);
+        Assert.Equal("$ACCEPT : . start $END; start : . a bvoid c; a : . 'A'; start : . bvoid; bvoid : . void; bvoid : . 'B'; void : . [$END]", nodes[0].ToString());
+        Assert.Equal("$ACCEPT : start . $END", nodes[1].ToString());
+        Assert.Equal("$ACCEPT : start $END .", nodes[2].ToString());
+        Assert.Equal("start : a . bvoid c; bvoid : . void; bvoid : . 'B'; void : . ['C']", nodes[3].ToString());
+        Assert.Equal("start : a bvoid . c; c : . 'C'", nodes[4].ToString());
+        Assert.Equal("start : a bvoid c . [$END]", nodes[5].ToString());
+        Assert.Equal("c : 'C' . [$END]", nodes[6].ToString());
+        Assert.Equal("bvoid : void . ['C', $END]", nodes[7].ToString());
+        Assert.Equal("bvoid : 'B' . ['C', $END]", nodes[8].ToString());
+        Assert.Equal("a : 'A' . ['B', 'C', $END]", nodes[9].ToString());
+        Assert.Equal("start : bvoid . [$END]", nodes[10].ToString());
     }
 }
