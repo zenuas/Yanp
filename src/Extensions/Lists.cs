0ab76b894270547d32fbaa878d9c960c6a3ed8a1
zenuas@gmail.com <zenuas@gmail.com>
2023-03-03 21:04:50 +0900

    Dictionary拡張を分離

--- src/Extensions/Lists.cs
+++ src/Extensions/Lists.cs
@@ -192,21 +192,4 @@ public static class Lists
 
     [DebuggerHidden]
     public static IEnumerable<T> Except<T>(this IEnumerable<T> self, IEnumerable<T> second, Func<T?, T?, bool> f) => Enumerable.Except(self, second, new EqualityComparerBinder<T>() { Equals = f });
-
-    [DebuggerHidden]
-    public static TValue GetOrNew<TKey, TValue>(this Dictionary<TKey, TValue> self, TKey key)
-        where TKey : notnull
-        where TValue : new()
-    {
-        if (!self.ContainsKey(key)) self.Add(key, new());
-        return self[key];
-    }
-
-    [DebuggerHidden]
-    public static TValue? GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> self, TKey key)
-        where TKey : notnull
-        where TValue : notnull
-    {
-        return self.TryGetValue(key, out var value) ? value : default;
-    }
 }
